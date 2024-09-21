3e1df5ea7979a27eb796a0e6ee4a7dd2a0fb8516
abdojat <3oooo3.2003@gmail.com>
2024-09-21 04:18:45 +0300

    second try

--- app.js
+++ app.js
@@ -31,10 +31,7 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-app.use(cors({
-    origin: 'https://keeperapp-o7lz.onrender.com/',
-    credentials: true
-}));
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
